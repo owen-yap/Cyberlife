654dc84fe151f1dcde3d8ff608f49c6d9b2ee2e1
owen-yap <owen.y.yuhan@gmail.com>
2022-07-15 15:34:44 +0800

    barebones and stream

--- lib/screens/joint_motor_function.dart
+++ lib/screens/joint_motor_function.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: file_names
 
+import 'dart:async';
+
 import 'package:cyberlife/widgets/appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
@@ -15,22 +17,44 @@ class JointMotorFunction extends StatefulWidget {
 
 class _JointMotorFunctionState extends State<JointMotorFunction> {
 
+  List<double>? _userAccelerometerValues;
+  final _jointMotorFunctionTest = <StreamSubscription<dynamic>>[];
+
   @override
   Widget build(BuildContext context) {
     CommonAppBar appBar = CommonAppBar(title: widget.title);
+
+    final userAccelerometer =
+    _userAccelerometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
+
     return Scaffold(
       appBar: appBar,
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            ElevatedButton(
-              child: const Text("Pain Map"),
-              onPressed: () {
-                gyroscopeEvents.listen((GyroscopeEvent event) {
-                  print(event);
-                });
-              },
+            DecoratedBox(
+                decoration: BoxDecoration(
+                  border: Border.all(width: 2.0, color: Colors.black38),
+
+                ),
+
+              child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child:
+                    SizedBox(
+                        width: 300.0,
+                        height: 100.0,
+                        child: Column(
+                          children: [
+                            Card(child: Text('X Value: ')),
+                            Card(child: Text('Y Value: ')),
+                            Card(child: Text('Z Value: ')),
+                          ]
+
+                        ),
+                    )
+
+              ),
             ),
           ],
         ),
@@ -38,3 +62,4 @@ class _JointMotorFunctionState extends State<JointMotorFunction> {
     );
   }
 }
+

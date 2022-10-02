b21b7bd57e7fb4ccd0f79a739f8dd66bc9178108
Justin Peng <justinpeng13@gmail.com>
2022-10-02 17:22:18 +0800

    Add download button and save to gallery function

--- lib/screens/pain_map.dart
+++ lib/screens/pain_map.dart
@@ -1,10 +1,15 @@
 // ignore_for_file: file_names
 
+import 'dart:typed_data';
+
 import 'package:cyberlife/widgets/appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:cyberlife/providers/pain_sticker_notification.dart';
 import 'package:cyberlife/widgets/pain_sticker.dart';
 import 'package:cyberlife/models/pain_sticker_list.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:screenshot/screenshot.dart';
 
 class PainMap extends StatefulWidget {
   const PainMap({Key? key}) : super(key: key);
@@ -16,6 +21,8 @@ class PainMap extends StatefulWidget {
 }
 
 class _PainMapState extends State<PainMap> {
+  //Create an instance of ScreenshotController
+  ScreenshotController screenshotController = ScreenshotController();
 
   PainStickerList psList = PainStickerList();
 
@@ -34,7 +41,11 @@ class _PainMapState extends State<PainMap> {
     });
   }
 
-
+  Future<void> saveImage(Uint8List bytes) async {
+    await [Permission.storage].request();
+    final result = await ImageGallerySaver.saveImage(bytes, name: "PainMap");
+    return result['filePath'];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,8 +62,11 @@ class _PainMapState extends State<PainMap> {
                   handleNotification(notification);
                   return true;
                 },
-                child: Stack(
-                  children: psList.generateList(),
+                child: Screenshot(
+                  controller: screenshotController,
+                  child: Stack(
+                    children: psList.generateList(),
+                  ),
                 ),
               ),
             ),
@@ -69,10 +83,10 @@ class _PainMapState extends State<PainMap> {
                             onTap: () {
                               _addSticker(Status.red, psList.length());
                             },
-                            child: const PainSticker(degree: Status.red, scale: 1.0, x: 0, y: 0, draggable: false),
+                            child: const PainSticker(degree: Status.red, scale: 2.0, x: 0, y: 0, draggable: false),
                           )
                       ),
-                      const Text("Sharp, Electric Pain", textAlign: TextAlign.center) // "Neuropathic pain: Sharp, Electric, Shooting, Stabbing"
+                      const Text("Sharp", textAlign: TextAlign.center) // "Neuropathic pain: Sharp, Electric, Shooting, Stabbing"
                     ],
                   ),
                   Column(
@@ -83,12 +97,22 @@ class _PainMapState extends State<PainMap> {
                           onTap: () {
                             _addSticker(Status.green, psList.length());
                           },
-                          child: const PainSticker(degree: Status.green, scale: 1.0, x: 0, y: 0, draggable: false),
+                          child: const PainSticker(degree: Status.green, scale: 2.0, x: 0, y: 0, draggable: false),
                         ),
                       ),
-                      const Text("Aching, Throbbing Pain", textAlign: TextAlign.center), // "Musculoskeletal pain"
+                      const Text("Aching", textAlign: TextAlign.center), // "Musculoskeletal pain"
                     ],
                   ),
+                  IconButton(
+                    icon: const Icon(Icons.download_rounded),
+                    iconSize: 48,
+                    tooltip: 'Save to gallery',
+                    onPressed: () async {
+                      final image = await screenshotController.capture();
+                      if (image == null) return;
+                      await saveImage(image);
+                    },
+                  )
                 ],
               ),
             ),

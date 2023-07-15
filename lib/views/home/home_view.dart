63d5871ae72627ab0c23406a7016903a4cfea214
owen-yap <owen.y.yuhan@gmail.com>
2023-07-15 23:04:50 +0800

    Add styling for 1 card

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -73,48 +73,207 @@ class _HomeViewState extends State<HomeView> {
               case ConnectionState.waiting:
                 return const CircularProgressIndicator();
               case ConnectionState.done:
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                      child: const Text("Pain Map"),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    PainMap(key: UniqueKey())));
-                      },
-                    ),
-                    ElevatedButton(
-                      child: const Text("Joint Motor Function"),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    const JointMotorFunctionInstructions()));
-                      },
-                    ),
-                    ElevatedButton(
-                      child: const Text("Open-Close Test"),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const OpenClose()));
-                      },
-                    ),
-                    ElevatedButton(
-                      child: const Text("Pinky Supination Test"),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const PinkySupination()));
-                      },
-                    ),
-                  ],
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          PainMap(key: UniqueKey())));
+                            },
+                            child: Column(
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.all(10),
+                                  height: 180.0,
+                                  width: 180,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(8),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        offset: const Offset(-6, 6),
+                                        blurRadius: 40,
+                                        spreadRadius: 1,
+                                        color: Colors.grey.withOpacity(0.1),
+                                      ),
+                                    ],
+                                  ),
+                                  child: Column(
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Container(
+                                            width: 70,
+                                            height: 70,
+                                            decoration: BoxDecoration(
+                                                color: Colors.white,
+                                                borderRadius:
+                                                    const BorderRadius.all(
+                                                        Radius.circular(50.0)),
+                                                border: Border.all(
+                                                  color: Colors.grey,
+                                                  width: 1.0,
+                                                )),
+                                            child: Center(
+                                              child: Container(
+                                                width: 60,
+                                                height: 60,
+                                                decoration: const BoxDecoration(
+                                                  color: Colors.white,
+                                                  image: DecorationImage(
+                                                    image: AssetImage(
+                                                        'assets/images/png/painMapIcon.png'),
+                                                    fit: BoxFit.scaleDown,
+                                                  ),
+                                                  borderRadius:
+                                                      BorderRadius.all(
+                                                          Radius.circular(
+                                                              50.0)),
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                          Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.end,
+                                            children: [
+                                              const Text(
+                                                '5 mins',
+                                                style: TextStyle(
+                                                    fontSize: 12,
+                                                    color: Colors.green,
+                                                    fontWeight:
+                                                        FontWeight.w600),
+                                              ),
+                                              const SizedBox(height: 4),
+                                              Image.asset(
+                                                'assets/images/png/clock.png',
+                                                height: 16,
+                                                width: 16,
+                                              ),
+                                            ],
+                                          )
+                                        ],
+                                      ),
+                                      const Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: [
+                                          Text('Pain Map',
+                                              style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold,
+                                              )),
+                                        ],
+                                      ),
+                                      Expanded(
+                                          child: Container(
+                                        color: Colors.white,
+                                      )),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.end,
+                                        children: [
+                                          const Text(
+                                            'Start Test',
+                                            style: TextStyle(
+                                              color: Colors.green,
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                          ),
+                                          Image.asset(
+                                              'assets/images/png/nextArrowIcon.png',
+                                              height: 14,
+                                              width: 14),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          PainMap(key: UniqueKey())));
+                            },
+                            child: Container(
+                              height: 160.0,
+                              width: 160,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(8),
+                                boxShadow: [
+                                  BoxShadow(
+                                    offset: const Offset(-6, 6),
+                                    blurRadius: 40,
+                                    spreadRadius: 1,
+                                    color: Colors.grey.withOpacity(0.1),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      ElevatedButton(
+                        child: const Text("Pain Map"),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      PainMap(key: UniqueKey())));
+                        },
+                      ),
+                      ElevatedButton(
+                        child: const Text("Joint Motor Function"),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      const JointMotorFunctionInstructions()));
+                        },
+                      ),
+                      ElevatedButton(
+                        child: const Text("Open-Close Test"),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const OpenClose()));
+                        },
+                      ),
+                      ElevatedButton(
+                        child: const Text("Pinky Supination Test"),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      const PinkySupination()));
+                        },
+                      ),
+                    ],
+                  ),
                 );
               default:
                 return const CircularProgressIndicator();
